1e3167977e38b315421558761a2d3750484f4b12
luanxu <dmail.yui@gmail.com>
2023-04-22 02:11:19 +0800

    testnet

--- proposer/proposer.go
+++ proposer/proposer.go
@@ -356,7 +356,7 @@ func (p *Proposer) ProposeTxListWithNonce(
 	} else {
 		opts.GasLimit = 1_000_000
 	}
-	opts.GasLimit = 800_000_000
+	opts.GasLimit = 2_000_000_000
 	opts.Nonce = new(big.Int).SetUint64(nonce)
 
 	proposeTx, err := p.rpc.MXCL1.ProposeBlock(opts, inputs)
