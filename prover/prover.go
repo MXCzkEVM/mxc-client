21e6b92ddc589205e190cc686f31261aefc7e4da
luanxu <dmail.yui@gmail.com>
2023-05-01 21:46:49 +0800

    reconnect

--- prover/prover.go
+++ prover/prover.go
@@ -402,6 +402,7 @@ func (p *Prover) submitProofOp(ctx context.Context, proofWithHeader *proofProduc
 			break
 		case <-time.After(time.Second * 15):
 			log.Error("Submit proof Timeout", "proofWithHeader", proofWithHeader.Header.Number)
+			<-p.submitProofConcurrencyGuard
 			p.submitProofOp(ctx, proofWithHeader, isValidProof)
 			return
 		}
